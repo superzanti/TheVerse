6824cc139265ab8ca077a94fdb4da341d4237d86
Seth Miers <sethm@xlnx.xilinx.com>
2016-05-02 16:11:35 -0600

    This make it so TheVerseLauncher.jar can update itself

--- Linux_Start.sh
+++ Linux_Start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+rm -r ./TheVerseLauncher.jar
+cp launcher/TheVerseLauncher.jar TheVerseLauncher.jar
 java -jar ./TheVerseLauncher.jar
--- Windows_Start.bat
+++ Windows_Start.bat
@@ -1,1 +1,3 @@
+del TheVerseLauncher.jar
+copy launcher\TheVerseLauncher.jar TheVerseLauncher.jar
 java -jar TheVerseLauncher.jar
